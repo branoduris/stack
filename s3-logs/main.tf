4aa7681b82573912f2083a5ef5da9d3d245056fb
Brano Duris <bduris@gmail.com>
2018-06-26 17:19:41 +0200

    add permission to write logs from ELB

--- s3-logs/main.tf
+++ s3-logs/main.tf
@@ -15,17 +15,41 @@ variable "logs_expiration_days" {
   default = 30
 }
 
-data "template_file" "policy" {
-  template = "${file("${path.module}/policy.json")}"
 
-  vars = {
-    bucket     = "${var.name}-${var.environment}-logs"
-    account_id = "${var.account_id}"
+data "aws_elb_service_account" "elb_account" {}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.name}-${var.environment}-logs/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_id}:root"]
+    }
+  }
+  statement {
+    actions   = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.name}-${var.environment}-logs/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["${data.aws_elb_service_account.elb_account.arn}"]
+    }
   }
 }
 
+
 resource "aws_s3_bucket" "logs" {
   bucket = "${var.name}-${var.environment}-logs"
+
+  acl = "log-delivery-write"
 
   lifecycle_rule {
     id = "logs-expiration"
@@ -42,7 +66,7 @@ resource "aws_s3_bucket" "logs" {
     Environment = "${var.environment}"
   }
 
-  policy = "${data.template_file.policy.rendered}"
+  policy = "${data.aws_iam_policy_document.s3_policy.json}"
 }
 
 output "id" {
